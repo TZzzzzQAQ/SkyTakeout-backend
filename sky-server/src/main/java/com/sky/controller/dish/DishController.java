e83c398ca2c0dc8595885c72aba5e619800e3940
TZzzzzQAQ <tzzzqaq@gmail.com>
2023-12-13 12:10:59 +1300

    v2.7.1：微小修改

--- sky-server/src/main/java/com/sky/controller/dish/DishController.java
+++ sky-server/src/main/java/com/sky/controller/dish/DishController.java
@@ -8,7 +8,6 @@ import com.sky.service.DishService;
 import com.sky.vo.DishVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
