36b1611f1b7aa3b3e682c55cd83cb10771d8d147
TZzzzzQAQ <tzzzqaq@gmail.com>
2023-12-26 09:13:31 +1300

    v7.3 修复了一些bug

---
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -0,0 +1,15 @@
+package com.sky.controller.user;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(tags = "Client订单接口")
+@Slf4j
+@RequestMapping("/user/order")
+public class OrderController {
+
+
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -0,0 +1,8 @@
+package com.sky.service.impl;
+
+import com.sky.service.OrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+}
